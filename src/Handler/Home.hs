866800d7b777f9c638f25f75a276a7e955efd902
maria antonia santosfontenelle <leticiafontenelle@yahoo.com.br>
2021-05-17 22:17:42 -0300

    Arquivos feitos em aula de Yesod

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -7,8 +7,40 @@
 module Handler.Home where
 
 import Import
-import Network.HTTP.Types.Status
-import Database.Persist.Postgresql
+import Text.Lucius
+import Text.Julius
+-- import Network.HTTP.Types.Status
+-- import Database.Persist.Postgresql
 
+-- static/img/logo.png => img_logo_png
+-- imagens baixadas
+
+getPage1R :: Handler Html
+getPage1R = do
+    defaultLayout $ do
+        [whamlet|
+            <h1>
+                PAGINA 1
+
+            <img src=@{StaticR img_logo_png}>        
+        |] 
+
+getPage2R :: Handler Html
+getPage2R = do
+    defaultLayout $ do
+        [whamlet|
+            <h1>
+                PAGINA 2
+        |] 
+
+-- Monad Handler => Back-end
+-- Monad Widget => Front-end
 getHomeR :: Handler Html
-getHomeR = undefined
+getHomeR = do
+    defaultLayout $ do
+        addStylesheet (StaticR css_bootstrap_css) 
+        toWidgetHead $(juliusFile "templates/home.julius")             
+        toWidgetHead $(luciusFile "templates/home.lucius")
+        $(whamletFile"templates/home.hamlet")
+             
+       
